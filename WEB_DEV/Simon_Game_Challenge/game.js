570b938b525dcac2d837d39eec8e3d2fed5654c6
riteshsaha <riteshsaha10@gmail.com>
2023-01-31 14:47:33 +0530

    jquery and Simon game challenge

---
+++ WEB_DEV/Simon_Game_Challenge/game.js
@@ -0,0 +1,68 @@
+//alert("Game started!");
+const buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+$(".btn").click(function () {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkSequence(userClickedPattern.length - 1);
+});
+
+$(document).keypress(function () {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    console.log(randomChosenColour);
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+function checkSequence(currentLevel) {
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        console.log("success");
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () { nextSequence(); }, 1000);
+        }
+    } else {
+        console.log("wrong");
+        playSound("wrong");
+        $("h1").text("Game Over, Press Any Key to Restart");
+        $("body").addClass("game-over");
+        setTimeout(function () { $("body").removeClass("game-over"); }, 200);
+        startOver();
+    }
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function () { $("#" + currentColour).removeClass("pressed"); }, 100);
+}
--- WEB_DEV/jQuery/index.js
+++ WEB_DEV/jQuery/index.js
@@ -1,1 +1,66 @@
-$("h1").css("color", "red");
+// $(document).ready() with a callback waits for the jquery to be executed before using the $("h1").css() call.
+$(document).ready(function () {
+    $("h1").css("color", "red"); // Incorrect way to assign style to elements.
+    $("h2").addClass("big-title"); // Correct way to seggregate having style properties in css instead of assigning property values to an element in js itself.
+    $("h1").addClass("big-title margin-50"); // Adding multiple classes
+    $("h1").removeClass("big-title"); // Removing one class; Can remove multiple classes also.
+    /*
+    $("h1").hasClass("big-title margin-50"); ----- false
+    $("h1").hasClass("margin-50"); ----- true
+    */
+    $("h2").text("HELLO!!!!");
+    $("button").text("Don't Click Me!"); // Changes text for all the buttons
+    $("button").html("<em>Click Me!</em>"); // Adds HTML content
+    $("a").attr("href", "https://www.yahoo.com"); // Changes attribute value
+    //$("h1").attr("class"); ---- margin-50
+
+    // Adding Event Listeners
+    $("button").click(function () {
+        if ($("h1").hasClass("big-title")) {
+            $("h1").removeClass("big-title");    
+        } else {
+            $("h1").addClass("big-title"); 
+        }
+    });
+
+    $("input").keypress(function (event) {
+        console.log(event.key);
+    });
+
+    /*
+    This allows to call the event listener when key is pressed when the page is loaded.
+    $(document).keypress(function (event) {
+        console.log(event.key);
+    });*/
+
+    // Another way to call event listener
+    $("h2").on("mouseover", function () {
+        if ($("h2").hasClass("margin-50")) {
+            $("h2").removeClass("margin-50");    
+        } else {
+            $("h2").addClass("margin-50"); 
+        }
+    });
+
+
+    $("h1").before("<button>Before</button>");
+    $("h1").after("<button>After</button>");
+    $("h1").prepend("<button>Prepend</button>");
+    $("h1").append("<button>Append</button>");
+
+    //$("button").remove();
+
+    $("button").on("click", function () {
+        $("h2").slideUp().slideDown().animate({opacity : 0.5});
+        //$("h2").animate({opacity: 0.3});
+        //$("h2").show();
+        //$("h2").hide();
+        //$("h2").toggle();
+        //$("h2").fadeIn();
+        //$("h2").fadeOut();
+        //$("h2").fadeToggle();
+        //$("h2").slideUp();
+        //$("h2").slideDown();
+        //$("h2").slideToggle();
+    })
+});
